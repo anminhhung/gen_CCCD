a24e403a8aa224a86e5e1a168a2e92874e29b12f
Hùng <anminhhung99@gmail.com>
2021-04-06 10:56:36 +0700

    update blur_bbox.py

--- utils/blur_bbox.py
+++ utils/blur_bbox.py
@@ -1,4 +1,40 @@
 import cv2 
+import os 
+import numpy as np 
+
+def get_boxes_classes(img_path, anno_path):
+    image = cv2.imread(img_path)
+    height, width, chanel = image.shape
+    colors = np.random.uniform(0, 255, size=(9, 3))
+    classes = ['So', 'HoTen', 'NgayThangSinh', 'GioiTinh', 'QuocTich', 'QueQuan', 'NoiThuongTruTren', 'NoiThuongTruDuoi', 'GiaTriDen']
+
+    with open(anno_path) as f:
+        boxes = f.read().split('\n')
+
+    list_bboxes = []
+    list_classes = []
+
+    for box in boxes:
+        if len(box)==0:
+            continue
+        cls = int(box.split()[0])
+        xc = int(float(box.split()[1]) * width)
+        yc = int(float(box.split()[2]) * height)
+        w = int(float(box.split()[3]) * width)
+        h = int(float(box.split()[4]) * height)
+
+        xmin = xc - w//2
+        ymin = yc - h//2
+        xmax = xmin + w
+        ymax = ymin + h
+
+        list_bboxes.append([xmin, ymin, xmax, ymax])
+        list_classes.append(classes[cls])
+
+        # image = cv2.rectangle(image, (xmin,ymin), (xmax,ymax), colors[cls])
+        # image = cv2.putText(image, classes[cls], (xmin, ymin), cv2.FONT_HERSHEY_PLAIN, 1, colors[cls], 2)
+
+    return image, list_bboxes, list_classes
 
 def blur_field(image, list_of_bbox):
     '''
@@ -17,8 +53,8 @@ def blur_field(image, list_of_bbox):
         image[bbox[1] : bbox[3], bbox[0] : bbox[2]] = field
 
     return image
-    
-def reduce_quality_texture(image, list_of_bbox, param = 50):
+
+def reduce_quality_texture(image, list_of_bbox, param=50):
     '''
       bbox format: x_min, y_min, x_max, y_max,
       list_of_bbox format: [
@@ -33,4 +69,18 @@ def reduce_quality_texture(image, list_of_bbox, param = 50):
         field = cv2.resize(field, (bbox[2] - bbox[0], bbox[3] - bbox[1]))
         image[bbox[1] : bbox[3], bbox[0] : bbox[2]] = field
 
-    return image
+    return image 
+
+if __name__ == '__main__':
+    root_dir = "Label2Gen"
+    image_name = "test"
+    image_path = os.path.join(root_dir, image_name + ".png")
+    annot_path = os.path.join(root_dir, image_name + ".txt")
+
+    image, list_bboxes, list_classes = get_boxes_classes(image_path, annot_path)
+
+    # new_image = blur_field(image, list_bboxes)
+    new_image = reduce_quality_texture(image, list_bboxes)
+
+    cv2.imshow("image", new_image)
+    cv2.waitKey(0)
